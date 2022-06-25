f15d93ac0ad877e0c8f7ad9524b03dc8f89296c1
Alper Umut Uçar <alperumutucar96@outlook.com>
2022-06-25 19:41:00 +0300

    # The true value of contract_address was given

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import {useEffect, useState} from "react";
 import abi from "./HeroesToken.json"
 
 
-const contract_address = '0x48a2e8684610a2F595365d7eaCe6d88ed8bc1815'
-const provider = new ethers.providers.JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc');
+const contract_address = '0x9e3F28C3c37ac77684730e223aa7c0621a206CD6'
+const provider = new ethers.providers.JsonRpcProvider('https://testnet.snowtrace.io/address/0x9e3F28C3c37ac77684730e223aa7c0621a206CD6');
 const contract = new ethers.Contract(contract_address, abi, provider);
 
 function App() {
