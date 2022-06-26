cf6902cbb675379fc17721636b96aefedf5819ea
Alper Umut Uçar <alperumutucar96@outlook.com>
2022-06-26 15:34:14 +0300

    # gelen dataların tabloya aktarılması alt yapısının kurulması

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import {ethers} from "ethers";
 import {useEffect, useState} from "react";
 import abi from "./HeroesToken.json"
-
+import {Characters} from "./Characters/Characters.tsx";
 
 const contract_address = '0x9e3F28C3c37ac77684730e223aa7c0621a206CD6'
 const provider = new ethers.providers.JsonRpcProvider('https://api.avax-test.network/ext/bc/C/rpc');
 const contract = new ethers.Contract(contract_address, abi, provider);
 
 function App() {
+    const [data, setData] = useState([])
     const [txs, setTxs] = useState([]);
+
+    const fetchData = (index) => {
+        fetch("https://character.heroesofnft.com/token/" + index)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setData(data)
+                console.log(data)
+            })
+    }
+
     useEffect(() => {
+        fetchData(1)
         console.log(contract)
     })
     return (
         <div className="App">
             <header className="App-header">
-
+                <Characters data={data}/>
             </header>
         </div>
     );
---
+++ src/Characters/Characters.tsx
@@ -0,0 +1,69 @@
+import {useEffect, useState} from "react";
+import {Table} from "antd";
+import {ColumnsType} from "antd/es/table";
+
+export type CharacterType = {
+    image: string;
+    name: string;
+    description: string;
+    id: number;
+    attributes: [];
+    character: string;
+}
+
+export function Characters(props: any) {
+    const columns: ColumnsType<CharacterType> = [
+        {
+            title: 'Image',
+            dataIndex: 'image',
+            key: 'image',
+            render: text => <img
+                height={100}
+                alt="image"
+                src={text}
+            />,
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: true,
+            render: (name) => `${name}`
+        },
+        {
+            title: 'Character',
+            dataIndex: 'character',
+            key: 'character',
+            sorter: true,
+            render: (name) => `${name}`
+        },
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            sorter: (a, b) => a.id - b.id,
+            key: 'id',
+        },
+        {
+            title: 'Description',
+            dataIndex: 'description',
+            key: 'description',
+        },
+        // {
+        //     title: 'Attributes',
+        //     dataIndex: 'attributes',
+        //     key: 'attributes',
+        //     render: <List></List>
+        // },
+    ];
+    useEffect(() => {
+
+    })
+
+    return (
+        <div>
+            <Table columns={columns}
+                   pagination={{pageSize: 5}}
+                   dataSource={props.data}/>
+        </div>
+    )
+}
